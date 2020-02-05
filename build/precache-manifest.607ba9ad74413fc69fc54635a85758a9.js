1451b264289a59ab7b65328cd1de6c080f2b56ba
aviavissar <aviavissar@gmail.com>
2020-02-05 07:34:18 +0200

    Add files via upload

---
+++ build/precache-manifest.607ba9ad74413fc69fc54635a85758a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dbb2d0eacaf4184c6a375f5397bdaf6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "792c7b0e2dc96b7b9fd6",
+    "url": "/static/js/2.6171863a.chunk.js"
+  },
+  {
+    "revision": "02f30225f121bded783630b22619f67f",
+    "url": "/static/js/2.6171863a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2d1a5942d8dad31d2a0d",
+    "url": "/static/js/main.a5a19748.chunk.js"
+  },
+  {
+    "revision": "c3e36adff0af932dd3b6",
+    "url": "/static/js/runtime-main.99c97ca2.js"
+  }
+]);
